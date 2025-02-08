92798e49d413968ac4a96b0d5d348905b5eba9ba
hamid-bassam <hamid.bassam.147@gmail.com>
2025-02-08 19:57:25 +0100

    mobile side projects display

--- src/app/_components/SideProjects.tsx
+++ src/app/_components/SideProjects.tsx
@@ -68,7 +68,7 @@ export const SideProject = (props: SideProjectProps) => {
                 <span className="bg-accent text-accent-foreground p-3 rounded-sm">
                     <props.logo />
                 </span>
-                <div className="relative w-full min-h-[6rem] h-full group">
+                <div className="relative w-full min-h-[6rem] h-full group max-sm:hidden">
                     <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-600 ease-in-out opacity-0 group-hover:opacity-100">
                         <Button variant={"secondary"}>Nouveau Contenu</Button>
                     </div>
@@ -76,6 +76,10 @@ export const SideProject = (props: SideProjectProps) => {
                         <p className="text-sm font-semibold">{t(props.title)}</p>
                         <p className="text-[0.7rem] text-muted-foreground">{t(props.description)}</p>
                     </div>
+                </div>
+                <div className="hidden max-sm:flex flex-col gap-2">
+                    <p className="text-sm font-semibold">{t(props.title)}</p>
+                    <p className="text-[0.7rem] text-muted-foreground">{t(props.description)}</p>
                 </div>
             </Link>
         </div>
